6f51868c046ab241d9aa18624d27bc1d67d3f374
thrisha2020 <your-email@example.com>
2025-03-11 20:02:32 +0530

    Added src.js

---
+++ src.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+      <div>
+        <h1>Welcome to Slot Booking System</h1>
+        <p>This is a simple React.js project.</p>
+      </div>
+    );
+  }
+  
+  export default App;
+  
